2dabba8099c9cbdb1649f15ebf3833c5d9f0c1d6
Hyeongcheol Kim <hyeongcheol@u.nus.edu>
2017-03-15 23:01:31 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,132 @@
 import React from 'react';
+
+//Component : Title 
+//Prop : todoCount
+const Title = React.createClass({	
+	render: function() {
+		  return (
+			<div>
+			   <div>
+				  <h1>to-do ({this.props.todoCount})</h1>
+			   </div>
+			</div>
+		 );
+	}
+});
+
+//Component: TodoForm
+//Prop : addTodo
+const TodoForm = React.createClass({
+	render: function() {
+		let input;
+		return (
+			    <form onSubmit={(e) => {
+					e.preventDefault();
+					this.props.addTodo(input.value);
+					input.value = '';
+				}}>
+					<input  ref={node => {input = node;}}/>
+					<br />
+				</form>
+		);
+	}
+});
+
+const Todo =  React.createClass({
+	render: function() {
+		 return (
+			<li>
+				<a href="#" className="list-group-item" onClick={() => {this.props.remove(this.props.todo.key)}}>ID :{this.props.todo.key} / Text : {this.props.todo.text}</a>
+			</li>
+		);
+	}
+});
+
+const TodoList = React.createClass({
+	render: function() {
+		// Map through the todos
+		const todoNode = this.props.todos.map((todo) => {
+			return (<Todo todo={todo} key={todo.key} remove={this.props.remove}/>);
+		});
+		
+		return (
+			<div className="list-group" style={{marginTop:'30px'}}>{todoNode}</div>
+		);
+  }
+}); 
+
+// Container Component
+// Todo Id
+window.id = 0;
+// Temporary array for local DB
+var todos = new Array();
+
+class TodoApp extends React.Component{
+	constructor(props){
+		// Pass props to parent class
+		super(props);
+		// Set initial state
+		this.state = {
+			data: []
+		}
+	}
+	
+	// Lifecycle method
+	// When the component is mounted, this method will be called automatically
+	// Please have a look at the site as below
+	// https://facebook.github.io/react/docs/react-component.html#componentdidmount
+	componentDidMount(){
+		// Make HTTP reques with Axios
+		// Set state with result
+		this.setState({data:todos});
+	}
+	
+	// Add todo handler
+	addTodo(val){
+		// Assemble data
+		const todo = {
+								text : val,
+								key : (new Date()).getTime()
+							  }
+		// Update data
+		todos.push(todo);
+		// Set the state value by using the local DB array
+		this.setState({data: todos});
+	}
+  
+	// Handle remove
+	handleRemove(key){
+		// Filter all todos except the one to be removed
+		const remainder = this.state.data.filter((todo) => {
+			if(todo.key !== key) return todo;
+		});
+	
+		// Replace the local DB array with new DB array excluding the removed item
+		todos = remainder;
+		// Update state with filter
+		this.setState({data: todos});
+	}
  
-class App extends React.Component {
-    render(){
- 
-        return (
-                <h1>Hello React Skeleton</h1>
-        );
-    }
+	render(){
+		// Render JSX
+		return (
+		<div>
+			<Title
+				//passing state date to a child as props
+				todoCount={this.state.data.length}
+			/>
+			<TodoForm
+				//passing state date to a child as props
+				addTodo={this.addTodo.bind(this)}
+			/>
+			<TodoList 
+				//passing state date to a child as props
+				todos={this.state.data} 
+				remove={this.handleRemove.bind(this)}
+			/>
+		</div>
+    );
+  }
 }
- 
-export default App;
+
+export default TodoApp;
